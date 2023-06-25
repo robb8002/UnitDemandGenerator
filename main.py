1e8759890d68cbde672c623d5af2d5ce297b25aa
Jessi Lanum <97244213+itsalljokay@users.noreply.github.com>
2023-06-25 18:54:57 -0400

    Basic structure work

---
+++ main.py
@@ -0,0 +1,32 @@
+#IMPORTS
+import numpy
+
+#VARIABLES
+"""
+Class 1: Food
+3 MREs per Marine per Day
+
+Class 2: Equipment
+
+
+Class 3: Fuel
+Class 5: Ammo
+Class 6: Personal Items
+Class 7: Major End Items
+Class 9: Repair Parts
+Class 10: Non Military Items
+"""
+competition_state_demand_mean = 2
+competition_state_demand_stdev = 1
+crisis_state_demand_mean = 3
+crisis_state_demand_stdev = 1
+war_state_demand_mean = 4
+war_state_demand_stdev = 1
+
+#DEMAND GENERATION
+"""
+Break each into functions.
+"""
+#Competition
+#Crisis
+#War
