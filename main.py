5c1b7365ec3c1e360e7fe6016cfb0c9d56a68532
Jessi Lanum <97244213+itsalljokay@users.noreply.github.com>
2023-06-26 09:24:01 -0400

    Variable definition using tuple unpacking

--- main.py
+++ main.py
@@ -1,27 +1,69 @@
+"""
+This one will be all classes of supply outputted for each state of existance.
+I dislike having so many variables defined, but I can't think of a slicker option...
+
+Attempting for the first time some tuple unpacking. 
+"""
 #IMPORTS
 import numpy
 
 #VARIABLES
 """
 Class 1: Food
-3 MREs per Marine per Day
-
-Class 2: Equipment
-
-
-Class 3: Fuel
-Class 5: Ammo
-Class 6: Personal Items
-Class 7: Major End Items
-Class 9: Repair Parts
-Class 10: Non Military Items
 """
-competition_state_demand_mean = 2
-competition_state_demand_stdev = 1
-crisis_state_demand_mean = 3
-crisis_state_demand_stdev = 1
-war_state_demand_mean = 4
-war_state_demand_stdev = 1
+competition_class_one_demand_mean, competition_class_one_demand_stdev = 2, 1
+crisis_class_one_demand_mean, crisis_class_one_demand_stdev = 3, 1
+war_class_one_demand_mean, war_class_one_demand_stdev = 4, 1
+
+"""
+Class 2: Equipment
+"""
+competition_class_two_demand_mean, competition_class_two_demand_stdev = 2, 1
+crisis_class_two_demand_mean, crisis_class_two_demand_stdev = 3, 1
+war_class_two_demand_mean, war_class_two_demand_stdev = 4, 1
+
+"""
+Class 3: Fuel
+"""
+competition_class_three_demand_mean, competition_class_three_demand_stdev = 4, 1
+crisis_class_three_demand_mean, crisis_class_three_demand_stdev = 3, 1
+war_class_three_demand_mean, war_class_three_demand_stdev = 4, 1
+
+"""
+Class 5: Ammo
+"""
+competition_class_five_demand_mean, competition_class_five_demand_stdev = 2, 1
+crisis_class_five_demand_mean, crisis_class_five_demand_stdev = 3, 1
+war_class_five_demand_mean, war_class_five_demand_stdev = 4, 1
+
+"""
+Class 6: Personal Items 
+"""
+competition_class_six_demand_mean, competition_class_six_demand_stdev = 2, 1
+crisis_class_six_demand_mean, crisis_class_six_demand_stdev = 3, 1
+war_class_six_demand_mean, war_class_six_demand_stdev = 4, 1
+
+"""
+Class 7: Major End Items
+"""
+competition_class_seven_demand_mean, competition_class_seven_demand_stdev = 2, 1
+crisis_class_seven_demand_mean, crisis_class_seven_demand_stdev = 3, 1
+war_class_seven_demand_mean, war_class_seven_demand_stdev = 4, 1
+
+"""
+Class 9: Repair Parts
+"""
+competition_class_nine_demand_mean, competition_class_nine_demand_stdev = 2, 1
+crisis_class_nine_demand_mean, crisis_class_nine_demand_stdev = 3, 1
+war_class_nine_demand_mean, war_class_nine_demand_stdev = 4, 1
+
+"""
+Class 10: Non-Military Items
+"""
+competition_class_ten_demand_mean, competition_class_ten_demand_stdev = 2, 1
+crisis_class_ten_demand_mean, crisis_class_ten_demand_stdev = 3, 1
+war_class_ten_demand_mean, war_class_ten_demand_stdev = 4, 1
+
 
 #DEMAND GENERATION
 """
